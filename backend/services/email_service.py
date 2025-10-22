03d4aeac02eee34339a99a3d686c2cf5a7e7e0b0
daniel etukudo <danielsamueletukudo@gmail.com>
2025-10-23 01:15:58 +0200

    fixed local host

--- backend/services/email_service.py
+++ backend/services/email_service.py
@@ -310,7 +310,7 @@ class EmailService:
         to_email: str, 
         enterprise_name: str, 
         inviter_name: str,
-        login_url: str = os.environ.get('FRONTEND_URL', 'http://localhost:3000') + "/accept-invitation"
+        login_url: str = os.environ.get('FRONTEND_URL', 'https://www.meallensai.com') + "/accept-invitation"
     ) -> bool:
         """
         Send an email to a user who was created by an organization admin
